b1d2172e904f314a83fc4402312f7b7a09994ff4
Bung <wndgh26@gmail.com>
2026-01-01 21:25:39 +0900

    ✨ feat: add Member entity and repository

---
+++ src/main/java/project/member/MemberRepository.java
@@ -0,0 +1,42 @@
+package project.member;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Log4j2
+@Repository
+public class MemberRepository {
+
+    private final Map<Long,Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+
+        return member;
+    }
+
+    public Member findById(Long id){
+
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+
+        return new ArrayList<>(store.values());
+    }
+
+    public void deleteMemberById(Long id){
+        store.remove(id);
+    }
+
+    public void clear(){
+        store.clear();
+    }
+}
