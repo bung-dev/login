6ba730ecea5e7e577d54e1222dbe17bcc93c08a4
bung-dev <wndgh26@gmail.com>
2026-01-06 17:55:03 +0900

    ♻️ refactor: refactor MemberController to use ResponseEntity

--- src/main/java/project/member/MemberController.java
+++ src/main/java/project/member/MemberController.java
@@ -4,39 +4,36 @@ package project.member;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public MemberResponse getMemberById(@PathVariable Long id) {
-        return memberService.getMemberById(id);
+    public ResponseEntity<MemberResponse> getMemberById(@PathVariable Long id) {
+        return ResponseEntity.ok(memberService.getMemberById(id));
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public MemberResponse createMember(@RequestBody @Valid MemberRequest request){
-
-        return memberService.createMember(request);
+    public ResponseEntity<MemberResponse> createMembers(@RequestBody @Valid MemberRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.createMember(request));
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<MemberResponse> getAllMembers(){
-        return memberService.getAllMembers();
+    public ResponseEntity<List<MemberResponse>> getAllMembers(){
+        return ResponseEntity.ok(memberService.getAllMembers());
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteMembers(@PathVariable Long id){
+    public ResponseEntity<Void> deleteMembers(@PathVariable Long id){
         memberService.deleteMember(id);
+        return ResponseEntity.noContent().build();
     }
 }
