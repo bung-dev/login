922fd41f1d1a7ab60b56b9c842670f71f525a226
bung-dev <wndgh26@gmail.com>
2026-01-06 17:43:38 +0900

    ♻️ refactor: add from method to convert entity to DTO

--- src/main/java/project/member/MemberRequest.java
+++ src/main/java/project/member/MemberRequest.java
@@ -5,4 +5,10 @@ import jakarta.validation.constraints.NotEmpty;
 public record MemberRequest(@NotEmpty String loginId,
                             @NotEmpty String password,
                             @NotEmpty String name) {
+
+    public static MemberRequest from(Member member){ //Entity -> DTO
+        return new MemberRequest(member.getLoginId(),
+                member.getPassword(),
+                member.getName());
+    }
 }
--- src/main/java/project/member/MemberResponse.java
+++ src/main/java/project/member/MemberResponse.java
@@ -5,4 +5,10 @@ import jakarta.validation.constraints.NotEmpty;
 public record MemberResponse(@NotEmpty String loginId,
                              @NotEmpty String password,
                              @NotEmpty String name) {
+
+    public static MemberResponse from(Member member){ //Entity -> DTO
+        return new MemberResponse(member.getLoginId(),
+                member.getPassword(),
+                member.getName());
+    }
 }
