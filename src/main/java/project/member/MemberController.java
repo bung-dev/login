345ccaa5754181859b00d217672fa28b512d95a4
bung-dev <wndgh26@gmail.com>
2026-01-07 23:55:56 +0900

    ✨ feat: implement session-based login

--- src/main/java/project/member/MemberController.java
+++ src/main/java/project/member/MemberController.java
@@ -1,8 +1,12 @@
 package project.member;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -29,14 +34,6 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.createMember(request));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<MemberResponse> loginMember(@RequestParam String loginId, @RequestParam String password){
-        MemberResponse login = memberService.login(loginId, password);
-        ResponseCookie cookie = getResponseCookie(login);
-
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(login);
-    }
-
     @GetMapping
     public ResponseEntity<List<MemberResponse>> getAllMembers(){
         return ResponseEntity.ok(memberService.getAllMembers());
@@ -46,6 +43,27 @@ public class MemberController {
     public ResponseEntity<Void> deleteMembers(@PathVariable Long id){
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MemberResponse> loginMember(@RequestParam String loginId, @RequestParam String password){
+        MemberResponse login = memberService.login(loginId, password);
+        ResponseCookie cookie = getResponseCookie(login);
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(login);
+    }
+
+
+    @PostMapping("/login/session")
+    public ResponseEntity<MemberResponse> loginSession(@RequestParam String loginId,
+                                                       @RequestParam String password,
+                                                       HttpServletRequest request){
+        MemberResponse login = memberService.login(loginId, password);
+        HttpSession httpSession = request.getSession();
+        httpSession.setAttribute("loginId", loginId);
+
+        return  ResponseEntity.ok().body(login);
+
     }
 
     private static @NonNull ResponseCookie getResponseCookie(MemberResponse login) {
