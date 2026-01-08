a2c2e03a4832ebde462678dae9c658e8e908dbdd
bung-dev <wndgh26@gmail.com>
2026-01-08 20:47:55 +0900

    ✨feat: register login interceptor in WebConfig

---
+++ src/main/java/project/WebConfig.java
@@ -0,0 +1,18 @@
+package project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.member.web.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/login","/members/logout","/css/**", "/error");
+    }
+}
