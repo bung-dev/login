816811bf340ef661f2581ee1410477bf6e2efb68
bung-dev <wndgh26@gmail.com>
2026-01-09 11:45:47 +0900

    ✨ feat: add UNAUTHORIZED error code

--- src/main/java/project/member/web/exception/ErrorCode.java
+++ src/main/java/project/member/web/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEMBER_404","존재하지 않는 회원입니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEMBER_404","존재하지 않는 회원입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "MEMBER_401", "인증되지 않은 요청입니다");
 
 
     private final HttpStatus status;
