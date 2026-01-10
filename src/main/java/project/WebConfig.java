308ef14fb60334c8034abcd69b71ab1b1d380870
bung-dev <wndgh26@gmail.com>
2026-01-10 18:36:32 +0900

    ✨ feat: allow unauthenticated access to member registration

--- src/main/java/project/WebConfig.java
+++ src/main/java/project/WebConfig.java
@@ -23,6 +23,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/login","/members/logout","/css/**", "/error");
+                .excludePathPatterns("/", "/members/login","/members/logout","/css/**", "/error","/members");
     }
 }
