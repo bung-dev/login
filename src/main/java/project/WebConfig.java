f1d02065e8ba6031842f7339937c3db3321c65b3
bung-dev <wndgh26@gmail.com>
2026-01-13 22:22:38 +0900

    🔧 chore: temporarily disable login interceptor

--- src/main/java/project/WebConfig.java
+++ src/main/java/project/WebConfig.java
@@ -18,11 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(new LoginMemberArgumentResolver());
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/login","/members/logout","/css/**", "/error","/members");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginCheckInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/", "/members/login","/members/logout","/css/**", "/error","/members");
+//    }
 }
