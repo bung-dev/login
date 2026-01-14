524a994231875bb0dda3bfe7a736cfa714ccaa69
bung-dev <wndgh26@gmail.com>
2026-01-14 23:41:30 +0900

    ♻️ refactor: add Role

--- src/main/java/project/member/domain/dto/MemberResponse.java
+++ src/main/java/project/member/domain/dto/MemberResponse.java
@@ -2,12 +2,15 @@ package project.member.domain.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import project.member.domain.Member;
+import project.member.domain.Role;
 
 public record MemberResponse(@NotEmpty String loginId,
-                             @NotEmpty String name) {
+                             @NotEmpty String name,
+                             @NotEmpty Role role) {
 
     public static MemberResponse from(Member member){ //Entity -> DTO
         return new MemberResponse(member.getLoginId(),
-                member.getName());
+                member.getName(),
+                member.getRole());
     }
 }
