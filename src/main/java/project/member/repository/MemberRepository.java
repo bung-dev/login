20c8d0fc110ec9d20a5d1d7300243f4a70e0f49f
bung-dev <wndgh26@gmail.com>
2026-01-14 23:42:04 +0900

    ✨ feat: add JpaMemberRepository

---
+++ src/main/java/project/member/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package project.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.member.domain.Member;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
+}
