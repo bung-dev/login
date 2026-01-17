3c81fe8c50721b43376c2053119fab71a4a4fbaa
bung-dev <wndgh26@gmail.com>
2026-01-17 12:06:54 +0900

    ✨ feat: add RestAccessDeniedHandler

---
+++ src/main/java/project/member/security/handler/RestAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package project.member.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import project.member.web.exception.ErrorCode;
+import project.member.web.exception.ErrorResponse;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.FORBIDDEN;
+
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorCode));
+
+        ErrorResponse body = ErrorResponse.of(errorCode.getCode(), errorCode.getMessage(), null);
+
+        objectMapper.writeValue(response.getWriter(), body);
+    }
+}
