bb36027e5221821206288d600ef9a1b7ed4af391
bung-dev <wndgh26@gmail.com>
2026-01-17 12:14:39 +0900

    ✨ feat: add FORBIDDEN error code

--- src/main/java/project/member/web/exception/ErrorCode.java
+++ src/main/java/project/member/web/exception/ErrorCode.java
@@ -10,10 +10,10 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEMBER_404","존재하지 않는 회원입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "MEMBER_401", "인증되지 않은 요청입니다"),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "MEMBER_401_INVALID", "아이디 또는 비밀번호가 올바르지 않습니다."),
-    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "MEMBER_409", "서버가 요청을 처리할 수 없습니다"),
+    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "MEMBER_409", "이미 사용중인 아이디입니다"),
 
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다.");
-
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_403", "접근 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
