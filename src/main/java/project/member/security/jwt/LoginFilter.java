3da38e160ed8c65c72f8906dc2e8947c1345e51a
bung-dev <wndgh26@gmail.com>
2026-01-20 11:17:31 +0900

    ✨ feat: add LoginFilter

---
+++ src/main/java/project/member/security/jwt/LoginFilter.java
@@ -0,0 +1,52 @@
+package project.member.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import project.member.security.CustomMemberDetails;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String loginId = request.getParameter("loginId");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken auth
+                = new UsernamePasswordAuthenticationToken(loginId, password);
+
+        return authenticationManager.authenticate(auth);
+
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        CustomMemberDetails customMemberDetails = (CustomMemberDetails) authResult.getPrincipal();
+        String loginId = customMemberDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority authority = iterator.next();
+
+        String role = authority.getAuthority();
+        String token = jwtUtil.createToken(loginId, role, 60 * 60 * 1000L);
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
+}
