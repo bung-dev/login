e9c65eea78e672737df18ce32796a189a06d6339
bung-dev <wndgh26@gmail.com>
2026-01-20 11:16:48 +0900

    ✨feat: add JWTUtil

---
+++ src/main/java/project/member/security/jwt/JWTUtil.java
@@ -0,0 +1,51 @@
+package project.member.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${jwt.secret}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getLoginId(String token) {
+        return getClaims(token).get("loginId",String.class);
+    }
+
+    public String getRole(String token) {
+        return getClaims(token).get("role",String.class);
+    }
+
+    public boolean isExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    public String createToken(String loginId, String  role, long expired) {
+        return Jwts.builder()
+                .claim("loginId", loginId)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expired))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
