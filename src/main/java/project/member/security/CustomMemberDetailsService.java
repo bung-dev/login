d393155219149a5f91ef64d4f8eb04a38e636fdb
bung-dev <wndgh26@gmail.com>
2026-01-20 11:12:34 +0900

    ♻️ refactor: add Log

--- src/main/java/project/member/security/CustomMemberDetailsService.java
+++ src/main/java/project/member/security/CustomMemberDetailsService.java
@@ -1,6 +1,7 @@
 package project.member.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Service;
 import project.member.domain.Member;
 import project.member.repository.MemberRepository;
 
+import java.util.Optional;
+
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class CustomMemberDetailsService implements UserDetailsService {
@@ -16,8 +20,12 @@ public class CustomMemberDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByLoginIdAndDeletedAtIsNull(username).orElseThrow(()
-                -> new UsernameNotFoundException("사용자를 찾을 수 없습니다" + username));
+        Optional<Member> memberOptional = memberRepository.findByLoginIdAndDeletedAtIsNull(username);
+
+        Member member = memberOptional.orElseThrow(() -> {
+            log.info("사용자를 찾을 수 없습니다: " + username);
+            return new UsernameNotFoundException("사용자를 찾을 수 없습니다" + username);
+        });
         return new CustomMemberDetails(member);
     }
 }
