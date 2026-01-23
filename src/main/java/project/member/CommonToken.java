257aab40e1c18a5908f12747b01397c93ba11ba5
bung-dev <wndgh26@gmail.com>
2026-01-23 11:54:03 +0900

    🐛 fix: correct CommonToken constant values

--- src/main/java/project/member/CommonToken.java
+++ src/main/java/project/member/CommonToken.java
@@ -2,8 +2,8 @@ package project.member;
 
 public class CommonToken {
     public final static long JWT_ACCESS_TOKEN_EXPIRED_TIME= 1000 * 60 * 5L; //15분
-    public final static long JWT_REFRESH_TOKEN_EXPIRED_TIME= 1000 * 60 * 5L; //1일
-    public final static int JWT_COOKIE_REFRESH_TOKEN_EXPIRED_TIME = 60 * 60 * 24; //1일
+    public final static long JWT_REFRESH_TOKEN_EXPIRED_TIME= 1000L * 60 * 60 * 24;; //1일(ms)
+    public final static int JWT_COOKIE_REFRESH_TOKEN_EXPIRED_TIME = 60 * 60 * 24; //1일(초)
 
     public final static String JWT_ACCESS_TOKEN_NAME = "access";
     public final static String JWT_REFRESH_TOKEN_NAME = "refresh";
