c8b5d74ed2be992294f0d2c2b87905425e8247d1
bung-dev <wndgh26@gmail.com>
2026-01-23 12:02:06 +0900

    ✨ feat: set refresh token in cookie on login

--- src/main/java/project/member/web/controller/AuthController.java
+++ src/main/java/project/member/web/controller/AuthController.java
@@ -1,7 +1,9 @@
 package project.member.web.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import project.member.domain.dto.LoginRequest;
 import project.member.domain.dto.TokenResponse;
 import project.member.service.AuthService;
 
+import static project.member.CommonToken.JWT_COOKIE_REFRESH_TOKEN_EXPIRED_TIME;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +21,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
         TokenResponse token = authService.login(request);
+        response.addHeader(HttpHeaders.SET_COOKIE, createCookie(token.refreshToken()));
         return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Bearer " + token.accessToken())
                 .body(token);
+    }
+
+    protected String createCookie(String value) {
+        return ResponseCookie.from("refreshToken", value)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(JWT_COOKIE_REFRESH_TOKEN_EXPIRED_TIME)
+                .build()
+                .toString();
     }
 }
