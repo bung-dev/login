d367e8d86c0c4fd7e7efaf58f1980efd1614543a
Bung <wndgh26@gmail.com>
2026-02-05 21:02:35 +0900

    ✨ feat: add OpenAPI configuration for Swagger

---
+++ src/main/java/project/member/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package project.member.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Login-Step-by-Step-Project API 목록")
+                        .description("단순 쿠키부터 OAuth2까지 단계별 로그인 구현 프로젝트 API 입니다")
+                        .version("0.1"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("개발용 서버")
+                ));
+    }
+}
